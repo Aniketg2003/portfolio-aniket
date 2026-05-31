3058111ed4bbab9d6c98a84feb1140c5daa14380
Aniket Gupta <aniketgupta25092003@gmail.com>
2026-05-31 21:16:17 +0530

    Create config.js with EmailJS configuration
    Added configuration constants for EmailJS.

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js - DO NOT COMMIT THIS FILE
+const CONFIG = {
+    EMAILJS_PUBLIC_KEY: "auqHQtNvGx3VxpbFo",
+    EMAILJS_SERVICE_ID: "service_iay19ba",
+    EMAILJS_TEMPLATE_ID: "template_tt8u96k"
+};
